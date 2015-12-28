0698cc4acf23b207324b98e951e4cb620e291d45
Miguh Ruiz <miguhruiz@gmail.com>
2015-12-28 16:00:50 +0100

    Added 'tags' support
    http://api.ghost.org/docs/tags

--- test/client-test.js
+++ test/client-test.js
@@ -4,6 +4,7 @@ var ghost = require('../')
 var Client = require('../lib/client')
 
 var url = 'http://api.ghost.test'
+var url2 = 'http://api.myghostblog.test'
 var clientId = 'ghost-frontend'
 var clientSecret = 'catsarecool'
 var token = 'Bearer 0X0X0X'
@@ -41,12 +42,46 @@ test('should list posts using user authentication', function (t) {
       'Authorization': token
     }
   })
-      .get('/posts')
-      .reply('200', [])
+    .get('/posts')
+    .reply('200', [])
 
   client.posts(function (err, posts) {
     t.error(err, 'should not be an error')
     t.ok(Array.isArray(posts), 'should be an array')
+    t.end()
+  })
+})
+
+test('should list tags using client authentication', function (t) {
+  var client = ghost.createClient({ endpoint: url2, clientId: 'ghost-frontend', clientSecret: clientSecret })
+  t.equals(typeof client.tags, 'function', 'should be a function')
+
+  nock(url2 + '/ghost/api/v0.1')
+      .get('/tags' + '?' + 'clientId=' + clientId + '&clientSecret=' + clientSecret)
+      .reply('200', [])
+
+  client.tags(function (err, tags) {
+    t.error(err, 'should not be an error')
+    t.ok(Array.isArray(tags), 'should be an array')
+    t.end()
+  })
+})
+
+test('should list tags using user authentication', function (t) {
+  var client = ghost.createClient({ endpoint: url, token: token })
+  t.equals(typeof client.tags, 'function', 'should be a function')
+
+  nock(url + '/ghost/api/v0.1', {
+    reqheaders: {
+      'Authorization': token
+    }
+  })
+  .get('/tags')
+  .reply('200', [])
+
+  client.tags(function (err, tags) {
+    t.error(err, 'should not be an error')
+    t.ok(Array.isArray(tags), 'should be an array')
     t.end()
   })
 })
