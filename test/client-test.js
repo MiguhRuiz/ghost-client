d09afa88c7652839efc3a459c4c51fded0ec1722
Miguh Ruiz <miguhruiz@gmail.com>
2015-12-28 19:39:09 +0100

    Added 'users' support
    http://api.ghost.org/docs/users

--- test/client-test.js
+++ test/client-test.js
@@ -3,8 +3,11 @@ var nock = require('nock')
 var ghost = require('../')
 var Client = require('../lib/client')
 
+// Created 3 url's to pass the test because of nock module specifications
 var url = 'http://api.ghost.test'
 var url2 = 'http://api.myghostblog.test'
+var url3 = 'http://api.ghostblog.test'
+
 var clientId = 'ghost-frontend'
 var clientSecret = 'catsarecool'
 var token = 'Bearer 0X0X0X'
@@ -82,6 +85,41 @@ test('should list tags using user authentication', function (t) {
   client.tags(function (err, tags) {
     t.error(err, 'should not be an error')
     t.ok(Array.isArray(tags), 'should be an array')
+    t.end()
+  })
+})
+
+test('should list users using client authentication', function (t) {
+  var client = ghost.createClient({ endpoint: url3, clientId: 'ghost-frontend', clientSecret: clientSecret })
+  t.equals(typeof client.users, 'function', 'should be a function')
+
+  nock(url3 + '/ghost/api/v0.1')
+      .persist()
+      .get('/users' + '?' + 'clientId=' + clientId + '&clientSecret=' + clientSecret)
+      .reply('200', [])
+
+  client.users(function (err, users) {
+    t.error(err, 'should not be an error')
+    t.ok(Array.isArray(users), 'should be an array')
+    t.end()
+  })
+})
+
+test('should list users using user authentication', function (t) {
+  var client = ghost.createClient({ endpoint: url3, token: token })
+  t.equals(typeof client.users, 'function', 'should be a function')
+
+  nock(url3 + '/ghost/api/v0.1', {
+    reqheaders: {
+      'Authorization': token
+    }
+  })
+  .get('/users')
+  .reply('200', [])
+
+  client.users(function (err, users) {
+    t.error(err, 'should not be an error')
+    t.ok(Array.isArray(users), 'should be an array')
     t.end()
   })
 })
