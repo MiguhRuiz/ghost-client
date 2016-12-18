6d9e34594df772b47cc66a2092b1042c099ea488
Miguh Ruiz <miguhruiz@gmail.com>
2016-12-18 15:46:13 +0100

    [Add] Module startup

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// This is where the module should be initialized
+var Module = require('./lib/index')
+
+function start (params) {
+  return new Module(params)
+}
+
+module.exports = start
--- tests/index.js
+++ tests/index.js
@@ -1,1 +1,6 @@
-// Here is where the test should be
+import test from 'ava'
+import Client from '../index'
+test('The module should be initialized', async t => {
+  const client = new Client()
+  t.truthy(client, 'should exist')
+})
