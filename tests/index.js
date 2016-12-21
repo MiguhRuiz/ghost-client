722a5233520248fbba3d0be9d0b8e9f3b7fff445
Miguh Ruiz <miguhruiz@gmail.com>
2016-12-21 13:20:41 +0100

    [Add] Posts function and test

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,31 @@
 import test from 'ava'
+import nock from 'nock'
+
 import Client from '../index'
+
+const token = 'mybeautytokenxd'
+const endpoint = 'http://api.ghost.test/ghost/api/v0.1'
+const opts = {
+  reqheaders: {
+    'Authorization': token
+  }
+}
+
 test('The module should be initialized', async t => {
-  const client = new Client()
-  t.truthy(client, 'should exist')
+    let client = new Client()
+    t.truthy(client, 'should exist')
+})
+
+test('Should return all posts using token authentication', async t => {
+  let ghost = new Client({
+    domain: endpoint,
+    token
+  })
+
+  nock(endpoint, opts)
+        .get('/posts')
+        .reply(200, [])
+
+  ghost.posts()
+        .then(posts => t.is(typeof posts, Array, 'The response is an array'))
 })
